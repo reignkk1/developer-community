b1366dee86ebbd3d14f8ed9c5b052288c403fc5a
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2023-03-09 18:53:27 +0900

    fix

--- client/src/axios.tsx
+++ client/src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  "https://port-0-board-server-6g2llexw0nts.sel3.cloudtype.app/";
+  "https://port-0-board-server-6g2llexw0nts.sel3.cloudtype.app";
 
 export function articleGet(page: string) {
   return axios.get(`/article/${page}`, { withCredentials: true });
